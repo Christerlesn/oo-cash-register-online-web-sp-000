30a8d3471e7107a389ee3d94942871bb6e505556
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 18:10:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,8 @@ class CashRegister
     if quantity>1
       i=0
       while i<quantity
-      @items << title
-      i+=1
+        @items << title
+        i+=1
       end
     else
       @items << title
@@ -23,8 +23,22 @@ class CashRegister
     @total += price*quantity
     @last_transaction_amount = @total
     @total
-    end
+  end
 
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+
+def void_last_transaction()
+  @total -= @last_transaction_amount
+end
 
 end #end of class CashRegister
 #binding.pry
