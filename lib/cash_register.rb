487bb04e4a8e78d1f10a1f2ae2a23946cb215883
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 17:07:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :discount, :total
+
+  def initialize(total = 0, *discount)
+    @total = total
+    discount = 20
+    @discount = discount
+  end
+
+  def add_item(title, price, *quantity)
+
+  end
+
+end #end of class CashRegister
+#binding.pry
