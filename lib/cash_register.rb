d01be1710608b723df854e372efb3bd97babeaed
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 18:38:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,18 +11,7 @@ class CashRegister
   end
 
   def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
+    @total += price * quantity
   end
 
   def apply_discount()
