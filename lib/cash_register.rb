92b51ae8db70ab27011f2607f27a763d1314aeeb
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 18:17:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,61 @@
-require 'pry'
+# require 'pry'
+# 
+# class CashRegister
+# 
+#   attr_accessor :discount, :total, :last_transaction_amount, :items
+# 
+#   def initialize(discount = 0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+# 
+#   def add_item(title,price,quantity = 1)
+#     if quantity>1
+#       i=0
+#       while i<quantity
+#         @items << title
+#         i+=1
+#       end
+#     else
+#       @items << title
+#     end
+#     @total += price*quantity
+#     @last_transaction_amount = @total
+#     @total
+#   end
+# 
+#   def apply_discount()
+#     if @discount > 0
+#       @discount = @discount/100.to_f
+#       @total = @total - (@total * (@discount))
+#       "After the discount, the total comes to $#{@total.to_i}."
+#     else
+#       "There is no discount to apply."
+#     end
+#   end
+# 
+# 
+# def void_last_transaction()
+#   @total -= @last_transaction_amount
+#   @items.pop
+# end
+# 
+# end #end of class CashRegister
+# #binding.pry
+
+
 
 class CashRegister
 
-  attr_accessor :discount, :total, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
+
 
   def add_item(title,price,quantity = 1)
     if quantity>1
@@ -36,10 +83,8 @@ class CashRegister
   end
 
 
-def void_last_transaction(last_transaction_amount)
-  @total -= @last_transaction_amount
-  @items.pop
-end
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
 
-end #end of class CashRegister
-#binding.pry
+end
