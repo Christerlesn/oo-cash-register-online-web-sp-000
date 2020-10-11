68038e879327b9b40cb898e7107c229c8fadcf26
Christerle Nounoute <christerlesn@gmail.com>
2020-10-11 18:38:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,8 +23,8 @@ class CashRegister
 #       "There is no discount to apply."
 #     end
 #   end
-# 
-# 
+#
+#
 # def void_last_transaction
 #   self.total = self.total - self.last_transaction_amount
 # end
